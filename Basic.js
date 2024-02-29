0afd4115031e0a64e85d9c4888a87524e185a083
Vishal Sharma <785vishal443@gmail.com>
2024-02-29 21:39:21 +0530

    basic setup

---
+++ Basic.js
@@ -0,0 +1,26 @@
+// Javascript is client side scripting Language 
+// Around 98.8% Website of the world use Javascript for their client side Programming Language
+// This is one of the Most Popular language in the world.
+// Javascript and Java are two diff programming language
+
+// Javascript is dynamic Programming Language
+
+
+
+// There is three keyword in javascript that use for creating the variable
+// unlike Java Javascript does not need any type delcration
+
+// here a is variable name
+//  var is keyword 
+//  after qual 20 is value which a stored
+
+var a = 20;
+//  this console will be visible either in terminal or browser console
+console.log(a);
+
+var name = "Vishal";
+var lastName = 'Sharma'
+
+// Javascripr consider single quote and double in as a string only
+
+console.log( 'Name=>' , name , 'lastName=>' , lastName)
