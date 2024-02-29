8862f0ffd3231509f54acb913c899b7f2ff10f86
Vishal Sharma <785vishal443@gmail.com>
2024-02-29 22:23:20 +0530

    data types

--- Basic.js
+++ Basic.js
@@ -3,7 +3,7 @@
 // This is one of the Most Popular language in the world.
 // Javascript and Java are two diff programming language
 
-// Javascript is dynamic Programming Language
+// Javascript is dynamic type casting Programming Language
 
 
 
@@ -14,13 +14,50 @@
 //  var is keyword 
 //  after qual 20 is value which a stored
 
-var a = 20;
+
+// this is a variable declration and value assignment
+var a = 20; // here the value of a is number
 //  this console will be visible either in terminal or browser console
 console.log(a);
 
-var name = "Vishal";
+var firstName = "Vishal";
 var lastName = 'Sharma'
 
 // Javascripr consider single quote and double in as a string only
 
-console.log( 'Name=>' , name , 'lastName=>' , lastName)
+console.log( 'Name=>' , firstName , 'lastName=>' , lastName)
+
+
+console.log('before Type of a =>' ,typeof a)
+
+// re-assigning the value to the variable
+var a = 'Newton' // here we have reassigned the a variable with the value string
+
+console.log('After Type of a =>' ,typeof a, "Value of a" ,a);
+
+
+// re-assigning the value to the variable
+a = [1,2,3,4,5]
+console.log(a) // 
+
+// Declaring the variable and re-assigning the value to the variabl
+// how to check the type of variable  (typeof)
+// Javascript is dynamically  convert the type 
+//  console.log() // console => object , log=> method
+
+
+// Javascript Datatype
+// 1  String
+// 2. Number
+// 3. Boolean
+// 4. Object (Array)
+// 5. Bigint
+// 6. Sybmbol
+// 7. Null
+// 8. Undefined
+
+// 
+
+for(let i =0;i<10;i++) {
+    console.log(i)
+}
