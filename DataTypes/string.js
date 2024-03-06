3607f755da95b1a11f38f6f20ff9aac628bc96a3
Vishal Sharma <785vishal443@gmail.com>
2024-03-06 22:55:16 +0530

    string concatenation

--- DataTypes/string.js
+++ DataTypes/string.js
@@ -149,3 +149,57 @@ console.log(sentence.endsWith('datatype')) // true
  // includes
 
 
+ var set2 ='                                                attaa                  ';
+//  trim() this function will remove extraa spaces from the start and end of the string
+//
+
+console.log(set2 , 'after trim:',set2.trim('') , set2.trimStart() , set2.trimEnd())
+
+
+// includes ?? this method will return true if given string is exist inside the main string else it return false
+
+
+var quote = 'Learn and Grow , Keep doing hard workd until you get the Job';
+
+console.log(quote.includes('Keep'))  // true
+console.log(quote.includes('Keep1')) // false
+console.log(quote.includes('Learn and Grw')) // false
+
+
+
+// Note => If we use plus operator(+) in the combination of number and string  , pluse operator convert number into string  first and after that concantenate it
+
+var data = 2 + 'hello';  // = > '2'+ 'hello'= > '2hello'
+
+console.log(data);
+
+
+
+var tt = +("20" + "30" + "40") + +"120" + "230";
+
+console.log("tt=>", tt); // 
+
+
+
+
+
+var ttt = +(+"20" + +"30" + "0")+ "20" + "60"; // 
+
+var ttt2 = +'2+2+2'; //  NaN
+
+
+console.log( 'ttt=>', ttt) //  205002060
+
+
+
+
+// Minus convert numeric string into the  number
+
+var tts = -("20" + "30" + "50") - ("30" - "30" + 60);
+
+console.log("tts=>>", tts); //
+
+
+
+
+
