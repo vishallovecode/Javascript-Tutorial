70e53626127c79fd8abd22191e9b4189b7392f4a
Vishal Sharma <785vishal443@gmail.com>
2024-03-11 22:52:47 +0530

    some js pitfalss

--- Practise.js
+++ Practise.js
@@ -79,75 +79,75 @@ console.log('Vishal' < 'VISHAL') //
 
 
 
-// Number and BOOLEAN
-// here boolean will converted into number true => 1 , false => 0
-console.log(2 > true);  // =>   2>1
+// // Number and BOOLEAN
+// // here boolean will converted into number true => 1 , false => 0
+// console.log(2 > true);  // =>   2>1
 
-console.log( 0 >= false); // 0>= 0
-
-
-//  Boolean 
-// boolean will be converted int number 
+// console.log( 0 >= false); // 0>= 0
 
 
-console.log(true > false) //  1 >0
+// //  Boolean 
+// // boolean will be converted int number 
 
 
-// String and number
+// console.log(true > false) //  1 >0
 
 
-console.log('AAAAA' > 5);
+// // String and number
+
+
+// console.log('AAAAA' > 5);
 
 
 
-//  undefined =>it will be assigned to the variable which is decalred but not initialized
-//  We can assigned the undefined value to the  variable as well
+// //  undefined =>it will be assigned to the variable which is decalred but not initialized
+// //  We can assigned the undefined value to the  variable as well
 
 
-var  checker;
- var checker2 = undefined;
+// var  checker;
+//  var checker2 = undefined;
 
-console.log(checker) //  undefined
-console.log(checker2) //  undefined
-
-
-console.log(typeof undefined) // 'undefined'
-
- //conveting undefined into boolean always gives false;
-
- // null
-
- // important
- console.log(typeof null) // object
-
- // null and undefined are diff data types , null  is object and undefined is undefined datatypes
- // null will be assigned by the developer , but undefined can be assigned by developer but  javascript also assigned undefined
- // value to the uinitialized variable
+// console.log(checker) //  undefined
+// console.log(checker2) //  undefined
 
 
- // conveting  boolean 
+// console.log(typeof undefined) // 'undefined'
 
- console.log(Boolean(null)) // false
+//  //conveting undefined into boolean always gives false;
 
+//  // null
 
- // NaN => JS consider it as one of the value of number in js;
- // typeof NaN => 'number'
+//  // important
+//  console.log(typeof null) // object
 
- // BOOLEAN 
- console.log(Boolean(NaN)) // false
-
-
- // null  =>  primitive vs non-primitive
- // typeof null => object => primitive data types
-
-  var employee = {} // this is object 
-
-  console.log(employee instanceof Object) // true
-
-  console.log(null instanceof Object) //  false
+//  // null and undefined are diff data types , null  is object and undefined is undefined datatypes
+//  // null will be assigned by the developer , but undefined can be assigned by developer but  javascript also assigned undefined
+//  // value to the uinitialized variable
 
 
-  console.log(NaN instanceof Number); // false 
+//  // conveting  boolean 
+
+//  console.log(Boolean(null)) // false
+
+
+//  // NaN => JS consider it as one of the value of number in js;
+//  // typeof NaN => 'number'
+
+//  // BOOLEAN 
+//  console.log(Boolean(NaN)) // false
+
+
+//  // null  =>  primitive vs non-primitive
+//  // typeof null => object => primitive data types
+
+//   var employee = {} // this is object 
+
+//   console.log(employee instanceof Object) // true
+
+//   console.log(null instanceof Object) //  false
+
+//   console.log(23 instanceof Number); // true
+//   console.log(NaN instanceof Number); // false 
 
 
  
@@ -159,10 +159,73 @@ console.log(typeof undefined) // 'undefined'
 
 
 
-//   class A {
+// //   class A {
 
-//   }
+// //   }
 
-//   A a =  new A(); // A class contsructor
+// //   A a =  new A(); // A class contsructor
 
 
+// console.log(Number('1')) //  1
+
+// console.log(Number(false)) // 0 // 
+
+// console.log(Boolean('false')) //  here 'false'  is string which is truthy value Boolean 
+//  // will convert it into the true => true
+
+// console.log(Boolean(false)) // false
+
+// console.log('13'-11); //  2
+
+// console.log('2'/'2'); //1 
+
+
+// console.log(35 + -'3' -false + true + '80'); // 3380
+
+
+
+// // 
+
+//  var a = new String('Vishal');
+
+//  console.log(typeof a) // object
+
+
+//  var b =  String('Vishal'); // string
+
+//  console.log(typeof b) 
+
+
+
+ 
+// diff == and === 
+// equal operator(==)=> this will check the value only;
+
+// strict equal operator(===)=> this will check type and  value  
+
+
+console.log('checker@@@@@@@@@@@')
+
+console.log(true ==  1) //  true => 1 ==1 //  true
+
+console.log(false == null) //  false // special case (pitfall)
+
+console.log(null == undefined) // true  NULL=> 0 ==  UNDEFINED=>0 (special case)
+
+console.log(undefined ==  NaN) //  undefined =>0 ==NaN => FALSE
+
+console.log(undefined==0) //   false
+
+console.log(null<=1) //  0<=1 // true
+
+console.log(null <='hey') // 
+
+
+console.log(NaN ==  false) // NaN == 0 // FALSE
+
+console.log(undefined == 'undefined') // UNDEFINED MEANS NOTHING // FALSE
+
+console.log(null == 'null') // false
+
+
+console.log(null == 0); //  false
