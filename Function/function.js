5101e37a07d8b6277ec63961a40fced25157a9ad
Vishal Sharma <785vishal443@gmail.com>
2024-03-12 22:59:05 +0530

    function return

--- Function/function.js
+++ Function/function.js
@@ -37,6 +37,8 @@ multiplyByTwo(60)
 function divideByThree (a) {
     var result  =  a/3;
     return result;
+    // line no 41 code is unrechable , after return statement in functions code is not execute
+    console.log('hey')
 }
 
 var  two = divideByThree(4);
@@ -44,3 +46,18 @@ var  two = divideByThree(4);
 console.log(two);
 
 
+// if there is no return in js function than js return undefined
+
+function callMe () {
+ // default js return the undefined
+ console.log('Checker123')
+//  return undefined
+}
+
+
+var result  = callMe();
+console.log(result) // undefined
+
+
+
+console.log(callMe()); //  checker and undefined
