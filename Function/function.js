e9274d430bb1b9ae047c9ae835b4f9279d0aa2e0
Vishal Sharma <785vishal443@gmail.com>
2024-03-13 21:40:48 +0530

    default parameters

--- Function/function.js
+++ Function/function.js
@@ -61,3 +61,69 @@ console.log(result) // undefined
 
 
 console.log(callMe()); //  checker and undefined
+
+
+
+
+function getName () {
+    var b ='Vishal';
+    return 
+      b;
+}
+
+console.log('checking',getName()); // undefined
+
+
+
+function printData(data) {
+     console.log(data); // 
+}
+
+
+
+printData(true)// boolean
+printData(2)// number
+printData([1,2,3]); // array
+printData({a:2}); //  object
+printData("Hello") // string
+
+
+printData(); //  undefined
+
+printData(2,'Hello', 'Hi ', 'sahi' , 'hai') // 
+
+// if there is no argument passed respected to the declare parameter , 
+// that parameter value will be undefined
+
+
+
+// default parameters in javascript  function
+// if there argument passed corresponding to the params then params 
+// will get that value else it will get the default value
+
+
+// create a function which will take first argument as value and second as multiplier 
+// you need to return the multiply of this two,  if there is not multiplier passed as argument consider
+// that as 2
+
+function multiplyByNumberOr2(value , multiplier) {
+        return value*multiplier;
+}
+
+multiplyByNumberOr2(10 , 10) // 10
+
+var result  =multiplyByNumberOr2(10) // 
+console.log(result) //      NaN
+
+
+
+function multiplyByNumberOr3(value , multiplier=3) {
+    return value*multiplier;
+}
+
+var res = multiplyByNumberOr3(10 , 10) // 100
+console.log(res) // 100
+
+
+var result  =multiplyByNumberOr3(10) // 
+console.log(result) //      30
