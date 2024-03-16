8aa1ca0e772caab91209981acfa34ea4c9fda0e7
Vishal Sharma <785vishal443@gmail.com>
2024-03-16 22:11:37 +0530

    array pass by reference

--- DataTypes/basicArray.js
+++ DataTypes/basicArray.js
@@ -36,3 +36,28 @@ var employeeData = [] ;
 console.log(typeof employeeData) // 'object'
 
 
+var specialCharactesr = ['.', '%', '&', '*'];
+
+
+
+// Note: Type of array is object (non-primitive data type)
+
+
+// Array is non-primitive data type  , and non-primitive datatype is pass as reference(memory)
+// in any of function or at the time of assignment , so when you modified the pass value , 
+// original array will be modified
+
+function modifiedArray (arr) {
+for(var i=0;i<arr.length;i++) {
+    arr[i]= arr[i]%2==0 ?  true : false
+}
+console.log('1=>' , arr)  // [false, true , false, true, false , true, false]
+}
+var array = [1,2,3,4,5,6,7];
+modifiedArray(array) // here arguments "array" is pass by reference
+
+console.log('2=>' ,array); // [1, 2,3,4,5,6,7]
+
+
+
+// Dyanamic
