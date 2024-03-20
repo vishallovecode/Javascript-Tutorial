754bdf35a4185fddb7113b7588ae592d694c2a13
Vishal Sharma <785vishal443@gmail.com>
2024-03-20 22:10:43 +0530

    GEC

---
+++ Hoisting/index.js
@@ -0,0 +1,26 @@
+// console.log('Relax......😀')
+
+// Scope
+// Hoisting
+// TDZ(temporal dead zone)
+// block scope
+// functional scope
+// window 
+// this 
+// context
+// execution context
+// lexical scoping
+// lexical environment
+// code execution 
+// CallStack
+
+
+// window is object which is represent of global memory 
+var a = 30;
+
+console.log(a);
+console.log(window.a);
+
+var n1 = 50;
+
+console.log(n1)
