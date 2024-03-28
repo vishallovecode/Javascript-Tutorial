5d80c9226800ca613833cb5c39acabcf1e8d8cbd
Vishal Sharma <785vishal443@gmail.com>
2024-03-28 21:43:44 +0530

    Lexical scoping

--- Hoisting/function.js
+++ Hoisting/function.js
@@ -7,35 +7,56 @@
 
 // The code inside the function is executed in functional  executed context 
 //We can have multipke execution context but there will be only one GEC
+// var a  =60 ;
+// const b =40 ;
+// let c =50;
+// var globalVariable = `
+// Hey I am 
+// global 
+// variable`
+
+//  function print(res) {
+//    var firstName = 'Vishal'
+//    let lastName = 'Sharma'
+//    const message = `Hi, ${firstName} 
+//    ${lastName} 
+//     Your score is:  ${res}`
+//    console.log(message)
+//    console.log(globalVariable)
+//  }
+//  function add(x, y ,z,) {
+//     var result = x+y+z;
+//     print(result) 
+//     console.log('add end')
+//  }
+//  add(a ,b,c)
+//  console.log(a,b,c)
+
+
+
 
 
 
 var a  =60 ;
-
 const b =40 ;
-
 let c =50;
+var globalVariable = `
+Hey I am 
+global 
+variable`
 
+ function print() {
+console.log(globalVariable)
+console.log(x)
+console.log(result)
 
- function print(res) {
-   var firstName = 'Vishal'
-   let lastName = 'Sharma'
-   const message = `Hi, ${firstName} 
-   ${lastName} 
-    Your score is:  ${res}`
-   console.log(message)
- 
  }
-
  function add(x, y ,z,) {
     var result = x+y+z;
-    print(result) 
-    console.log('add end')
+    console.log(globalVariable)
+    print() 
  }
-
  add(a ,b,c)
-
-
  console.log(a,b,c)
 
 
