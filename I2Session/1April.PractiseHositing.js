cd538b553a3bb3adec2136b07614588a1907f4d0
Vishal Sharma <785vishal443@gmail.com>
2024-04-01 22:21:58 +0530

    practise -2

--- I2Session/1April.PractiseHositing.js
+++ I2Session/1April.PractiseHositing.js
@@ -1,18 +1,35 @@
-(
-    ()=>{
+// (
+//     ()=>{
 
-         var a =b =3
-         // = >
-         var a =3 ;
-         b=3;
-        //  var a =3 ;
-        //   var b =a
-        d =100
-    }
-)()
+//          var a =b =3
+//          // = >
+//          var a =3 ;
+//          b=3;
+//         //  var a =3 ;
+//         //   var b =a
+//         d =100
+//     }
+// )()
 
+// console.log(d)
+// console.log( b )
+// console.log(a)
+
+
+
+
+function openSurjeetDimag() {
+    var a =b=c=d=e=100;
+    let a1 = b1 =100
+    console.log(a1)
+}
+
+
+openSurjeetDimag() // 
+console.log(b1)
+console.log(b)
+console.log(c)
 console.log(d)
-console.log( b )
-console.log(a)
+console.log(e)
 
-
+// console.log(a1)
