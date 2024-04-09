eb70722b134db9cc8e1eb5b9bdf3258d29c71c74
Vishal Sharma <785vishal443@gmail.com>
2024-04-09 22:00:51 +0530

    dom tut-1

---
+++ DomManipulation/index.js
@@ -0,0 +1,6 @@
+function changeColor  () {
+    const header = document.getElementById('heading');
+    header.style.background = 'red';
+    header.style.color= 'Black'
+    header.style.borderRadius = '20px'
+}
