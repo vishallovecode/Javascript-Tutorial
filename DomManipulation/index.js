f50a2927dbdc00beaaabce99d83a1a530fe4f5fd
Vishal Sharma <785vishal443@gmail.com>
2024-04-09 22:46:58 +0530

    dom

--- DomManipulation/index.js
+++ DomManipulation/index.js
@@ -1,6 +1,26 @@
 function changeColor  () {
+    
     const header = document.getElementById('heading');
     header.style.background = 'red';
     header.style.color= 'Black'
     header.style.borderRadius = '20px'
 }
+
+// dom  => document object model 
+// When a browser loaded the html page  file in browser it will create Document  Object Model 
+// It is like tree of objects structure
+
+
+// Using dcoument
+// You can delete the element from page 
+// You can update the element from page
+// You can create new element into the page
+// You can add events(action) to the page
+// You cam remove add and edit css in web page
+// You can change the attributes of the element
+// You can do all the  crud operations in every attributes of html element
+
+
+// Document object model is use for dynamically changing the content of web page
+
+// The document object model is a platform or language neutral interface  that allows (any programming language) javacript to dynamicalyy  , add , delete , update ,edit the web page or html element
