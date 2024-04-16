303b1f479265f6d9e2068fbc9c741b4baa2c3f5f
Vishal Sharma <785vishal443@gmail.com>
2024-04-16 21:56:16 +0530

    getElementsByClassName

--- DomManipulation/Dom2/Dom2.js
+++ DomManipulation/Dom2/Dom2.js
@@ -7,6 +7,22 @@
 // accessing the dom element using getElementById
 
  function main() {
+    // it will written the single node
     const heading = document.getElementById('heading');
     heading.innerText = 'lets Rock'
+
+    // accessing the elements using getElementsByClassName
+    const elements =  document.getElementsByClassName('check'); //
+    console.log(elements)
+    for(let node of elements) {
+        node.style.border = "2px solid black"
+    }
+
+
+    const element1 = document.getElementById('check');
+    // console.log(element1)
  }
+
+
+ // Accessing the dom element using getElementsByClassName
+
