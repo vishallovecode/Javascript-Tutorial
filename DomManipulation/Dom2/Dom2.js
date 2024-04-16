a7d3d770ab7f707227e9706e11f4ffa84e52a733
Vishal Sharma <785vishal443@gmail.com>
2024-04-16 21:37:28 +0530

    dom manipulation , accessing the dome elements

---
+++ DomManipulation/Dom2/Dom2.js
@@ -0,0 +1,12 @@
+
+// accessing the dom elements
+// we have different ways to do so.
+
+// How to access the element of Dom
+
+// accessing the dom element using getElementById
+
+ function main() {
+    const heading = document.getElementById('heading');
+    heading.innerText = 'lets Rock'
+ }
