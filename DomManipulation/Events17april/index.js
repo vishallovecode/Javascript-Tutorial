027caa6d8deede2f7b129839e8f286f2738262c6
Vishal Sharma <785vishal443@gmail.com>
2024-04-17 22:26:21 +0530

    onclcik event listener

--- DomManipulation/Events17april/index.js
+++ DomManipulation/Events17april/index.js
@@ -11,3 +11,34 @@ function clickme2 () {
 function logger () {
     console.log('Hi Hi You just did it.')
 }
+
+// Circle 
+
+function createBigCircle () {
+    const divNode =  document.createElement('div'); // creating node 
+    divNode.id='bigCircle'
+    divNode.classList.add('circle'); // this is how you can add classname in dynamic css
+    divNode.classList.add('big');
+    document.body.appendChild(divNode) // here we have added divNode inside the body
+}
+
+
+function createMediumCircle () {
+    const divNode =  document.createElement('div'); // creating node 
+    divNode.id='mediumCircle'
+    divNode.classList.add('circle'); // this is how you can add classname in dynamic css
+    divNode.classList.add('medium');
+    const bigCircle = document.getElementById('bigCircle');
+    bigCircle.appendChild(divNode)
+}
+
+function createSmallCircle () {
+    const divNode =  document.createElement('div'); // creating node 
+    divNode.classList.add('circle'); // this is how you can add classname in dynamic css
+    divNode.classList.add('small');
+    divNode.textContent = 'Center';
+    divNode.classList.add('text');
+    const medCircle = document.getElementById('mediumCircle');
+    medCircle.appendChild(divNode)
+
+}
