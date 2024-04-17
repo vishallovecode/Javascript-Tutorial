97467a27619e60a3b28cf3e5a181bd03dba2dad3
Vishal Sharma <785vishal443@gmail.com>
2024-04-17 21:59:23 +0530

    Events

--- DomManipulation/Dom2/Dom2.js
+++ DomManipulation/Dom2/Dom2.js
@@ -9,7 +9,8 @@
  function main() {
     // it will written the single node
     const heading = document.getElementById('heading');
-    heading.innerText = 'lets Rock'
+    heading.innerText = 'lets Rock';
+
     // accessing the elements using getElementsByClassName
 
     const elements =  document.getElementsByClassName('check'); //
@@ -23,7 +24,6 @@
         } else {
             node.innerText = 'Odd'
         }
-       
     }
 
     const element1 = document.getElementById('check');
@@ -33,16 +33,16 @@
 
     // Finding the elements using Tag Name
 
-    const elem =  document.getElementsByTagName('h4')
+    const elem =  document.getElementsByTagName('h4') // 
     const elem1 =  document.getElementsByTagName('span')
 
     console.log('Tag name h4=>::' ,elem)
     console.log('Tag name span=>::' ,elem1)
 
     //querySelector this will return the single object , which match given conditon
-    const q1 = document.querySelector('.check')
-    const q2 = document.querySelector('#heading')
-    const q3 = document.querySelector('h4')
+    const q1 = document.querySelector('.check');
+    const q2 = document.querySelector('#heading');
+    const q3 = document.querySelector('h4');
     console.log(q1 , q2 , q3)
 
     // using css selectors
@@ -56,7 +56,33 @@
  }
 
 
- 
 
 
 
+ // append and appendChild
+
+ // appendChild is use for only adding the node
+
+ createNode()
+function createNode () {
+    const node =  document.createElement('h1');
+    const node1 =  document.createElement('h1');
+    node.innerText =  'This is created using appendChild';
+    node1.innerText =  'This is created using appendChild111';
+    // i want to attached this node as a child of body
+    document.body.append(node , node1);
+    document.body.appendChild(node , node1);
+    //In append method we can provide multiple node 
+    // In appendChild if we will give multi node params it will add only first params
+
+    const node12 =  document.createElement('h1');
+    node12.append('With append method')
+    // append methods will work for the domstring and node as well.
+    document.body.appendChild(node12 );
+    // appendChild always required the params as node 
+    node12.appendChild('With append method1234')
+
+}
+
+
+// appendChild and append =>This function can we access through any node 
---
+++ DomManipulation/Events17april/index.js
@@ -0,0 +1,13 @@
+function clickMe1 () {
+  alert('Hey I am just clicked by you')
+}
+
+
+function clickme2 () {
+    const div =  document.getElementById('clickme1');
+    div.style.display = 'none'
+}
+
+function logger () {
+    console.log('Hi Hi You just did it.')
+}
