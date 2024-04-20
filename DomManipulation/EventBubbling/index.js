4832b027a4ffd46faf9906353c516e56c1034b60
Vishal Sharma <785vishal443@gmail.com>
2024-04-20 22:42:32 +0530

    event bubbling use , stopPropagation , event delegation

--- DomManipulation/EventBubbling/index.js
+++ DomManipulation/EventBubbling/index.js
@@ -3,4 +3,31 @@
 
 // Event bubbling => bottom => top (document) (default)
 // Event bubbling => Whenever click or any events happen into the child element the events bubbles up towards its parent till the top most parent node document it called event bubbling.
+
+// Event bubbles use case
+// how can we stop this  
+
 // Event capturing => top => bottom (we need to tell browser i want capturing)
+
+function grandparent (e) {
+    console.log('======grandparent========')
+        console.log('e.target=>', e.target)
+        console.log('e.currentTarget=>',e.currentTarget)
+}
+
+
+function grandchild (e) {
+    console.log('======grandchild========')
+    console.log('e.target=>', e.target)
+    console.log('e.currentTarget=>',e.currentTarget)
+    e.stopPropagation();
+}
+
+
+
+
+function child (e) {
+    console.log('======child========')
+    console.log('e.target=>', e.target)
+    console.log('e.currentTarget=>',e.currentTarget)
+}
