9e65f0f5c4e4f2afba542edb02b3d885898e9fa1
Vishal Sharma <785vishal443@gmail.com>
2024-04-24 22:26:16 +0530

    Object.keys() , Object.values() ,Object.entrie()

--- OOPS/index.js
+++ OOPS/index.js
@@ -60,5 +60,53 @@ console.log(employee)
  var dynamicKey2= 'Bank Balance';
 console.log(employee[dynamicKey1]) // =>console.log(employee['orgName']) 
 
-console.log(employee[dynamicKey2]) // =>console.log(employee['Bank Balance']) 
+console.log(employee[dynamicKey2]) // =>console.log(employee['Bank Balance'])
+
+
+// Creating the object uisng Object Constructor
+
+console.log(`%cObject Creation with constuctor`,'color: #00ff33; font-size: 20px')
+
+var person  = new Object({name: 'xyz' , lastName: 'rat'});
+
+console.log(person)
+
+
+console.log(`%c How to get all  the keys of object`,'color: blue; font-size: 20px')
+
+ //  Object.keys() => this will accept the object as a argument and return the array of object keys
+
+ var employee = {
+    name: 'Vishal Sharma',
+    orgName: 'Newton School',
+    address: 'Kanpur',
+    email:'785vishal443@gmail.com',
+    'Bank Balance': 100,
+ }
+
+var keys =  Object.keys(employee);
+console.log(keys) //  ['name', 'orgName', 'address', 'email' , 'Bank Balance']
+
+console.log(`%c How to get all  the value of object`,'color: yellow; font-size: 20px')
+
+  // Object.values() => this will accept the object as a argument and return the array of object values
+
+  var values =  Object.values(employee);
+  console.log(values);
+
+
+   // ##############################  Object.entries() #######################
+
+var entries  = Object.entries(employee); 
+console.log('entries' , entries)
+
+entries.forEach((elem , index)=>{
+    console.log('key=>' , elem[0] , 'value=>' , elem[1])
+})
+
+
+
+
+
+
 
