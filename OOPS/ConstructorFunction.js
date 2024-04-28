207c22c4d8536910cfbf93d030e8dffdaa7625ff
Vishal Sharma <785vishal443@gmail.com>
2024-04-28 12:26:13 +0530

    Constructor Function return

--- OOPS/ConstructorFunction.js
+++ OOPS/ConstructorFunction.js
@@ -1,40 +1,89 @@
-console.log('Hey We are learning , Construtor Function here');
+// console.log('Hey We are learning , Construtor Function here');
 
-// Constructor Function can be created only in normal function 
-// why ?? because normal function have it own context (this)
-// best practise to create constructor function always start with capital letter
+// // Constructor Function can be created only in normal function 
+// // why ?? because normal function have it own context (this)
+// // best practise to create constructor function always start with capital letter
 
-const number =  new Number() // Number is constructor function 
+// const number =  new Number() // Number is constructor function 
 
 
-var employee = {
-    salary: 3000000,
-    taxRegime: 'New',
-    name: 'XYZ',
-    adress: 'XYZ PRS',
-    donation:  500000,
-    investmnent: 50000,
-    hra: 300000
+// var employee = {
+//     salary: 3000000,
+//     taxRegime: 'New',
+//     name: 'XYZ',
+//     adress: 'XYZ PRS',
+//     donation:  500000,
+//     investmnent: 50000,
+//     hra: 300000
+// }
+
+// // const emp1 = {...employee};
+
+
+// function Employee () {
+//     // here this is empty object we are setting the key value pair here  ,
+//     // constructor function return this by default
+//   this.firstName = 'Vishal',
+//   this.lastName= 'Sharma';
+//   this.salary = 200000;
+//   this.adress = 'Azad Nagar kanpur'
+// }
+// const empl  = new Employee();
+// const empl1  = new Employee();
+// const empl2  = new Employee();
+// const empl3  = new Employee();
+
+// empl3.adress ='Delhi Gurgaon'
+// console.log(empl , empl1 ,empl2 , empl3);;
+
+
+
+// // Constructor  function are exactly same as the normal functions  but 
+// // they some rules 
+
+// // They should start with the capital letter
+// // At the time of function call they should call using new keyword
+
+
+// // Whenever you call the constrcutor function  , first empty object will be initialzed to the this 
+
+// function Person (name , lastName , adress , gender) {
+//     //  this = {} Done by js
+//     this.firstName = name;
+//     this.lastName = lastName
+//     this.adress =  adress;
+//     this.gender = gender;
+//     // Return will be by default this ?? 
+// }
+
+
+
+
+
+// const per = Person('1' ,'2','3','4');
+// const per1 = Person('11' ,'2','3','4')
+
+
+// Return  of constructor function 
+
+// Constructor Function always return the this 
+// if we return the object then constructor function return object
+// If we try to return any value apart from the Object(function , array  , object) this  will return the this only
+
+
+function Student () {
+    // return  'Hey'
+    // this = {}
+    console.log('this' , this) //  {name: 'Raj'}
+     this.name = 'Raj'
+    return this;
 }
 
-// const emp1 = {...employee};
 
+// console.log(Student())
 
-function Employee () {
-    // here this is empty object we are setting the key value pair here  ,
-    // constructor function return this by default
-  this.firstName = 'Vishal',
-  this.lastName= 'Sharma';
-  this.salary = 200000;
-  this.adress = 'Azad Nagar kanpur'
-}
-const empl  = new Employee();
-const empl1  = new Employee();
-const empl2  = new Employee();
-const empl3  = new Employee();
-
-empl3.adress ='Delhi Gurgaon'
-console.log(empl , empl1 ,empl2 , empl3);;
+const st = new Student();
+console.log(st) //  {}
 
 
 
