c750b4023bc4f5665ff04660db696d43f62be2a8
Vishal Sharma <785vishal443@gmail.com>
2024-05-01 14:17:28 +0530

    THIS ARROW

--- OOPS/this.js
+++ OOPS/this.js
@@ -1,20 +1,21 @@
-// // "use strict"; 
-// // In Javascript this will refers to the object 
-// // The this keyword refers to the context  where a piece of code  , such as function body supposed to run
+// // // "use strict"; 
+// // // In Javascript this will refers to the object 
+// // // The this keyword refers to the context  where a piece of code  , 
+// //    such as function body supposed to run
 
-// console.log(this) //  GEC
+// // console.log(this) //  GEC
 
-// function callRajaBabu() {
-//     console.log('inside raja babu' ,this)
-// }
+// // function callRajaBabu() {
+// //     console.log('inside raja babu' ,this)
+// // }
 
 
-// callRajaBabu();
+// // callRajaBabu();
 
 // function fatheroffather () {
 //     let name = 'baccha yada'
 //     function baccha() {
-//         console.log('baccah yadav calling' , this)
+//         console.log('baccah yadav calling' , this)  // this => window (non strict mode ) 
 //     }
 //     baccha()
 // }
@@ -24,55 +25,132 @@
 
 
 
-// the value of this will does not depend where it is delcared it depends where the functions or method is invoked
+// // the value of this will does not depend where it is delcared it depends where the functions or method is invoked
 
 
-// const student = {
-//     firstName: 'Lohit ka babu(male version age 26)',
-//     lastName: 'Lohit',
-//     getLohitBaby: function() {
-//         console.log('Hey i am lohit ka beba...' , this);
+// // const student = {
+// //     firstName: 'Lohit ka babu(male version age 26)',
+// //     lastName: 'Lohit',
+// //     getLohitBaby: function() {
+// //         console.log('Hey i am lohit ka beba...' , this);
+// //     },
+// //     getFullName() {
+// //         console.log('I am fullname' , this);
+// //     }
+// // }
+// // // student.getLohitBaby();
+// // // student.getFullName();
+
+
+
+// // //  student.getLohitBaby();
+
+// // student.getFullName(); // GEC => THIS  => STUDENT (method invoked)
+// // const func1 = student.getLohitBaby;
+// // func1(); // GEC =< THIS => WINDOW (function call )
+
+// //Note-> 1  Normal Function (This concepts is only for remebering this value)
+// // IF FUNCTION CALL THEN THIS  => WINDOW 
+// // IF METHOD INVOKED THEN "THIS" WILL BE THE VALUE BEFORE DOT 
+
+
+
+// var employee = {
+//     firstName: 'Radar ',
+//     lastName: 'Yuirt',
+//     salary: 230000,
+//     getSalary: function() {
+//       console.log( this.salary) // ?? 
+//       function call2 () {
+//         console.log('inside getSalary salary' , this ) // ?? 
+//       }
+//       call2();
 //     },
-//     getFullName() {
-//         console.log('I am fullname' , this);
+//     getFullName() { 
+//         console.log(this.firstName +  this.lastName)  // ??  this => employee
+//         function call1 () {
+//             console.log('Function inside the getFullName' , this) // ??
+//         }
+//         call1() //  this is the function call => this  => window (not strict mode in strict mode it will  be undefined)
 //     }
 // }
-// // student.getLohitBaby();
-// // student.getFullName();
+
+// employee.getFullName();
+// employee.getSalary();
 
 
 
-// //  student.getLohitBaby();
+// // "This" value in arrow function will always point to the it parent normal function context (this) if there is 
+// // no parent normal function then it point to the window
 
-// student.getFullName(); // GEC => THIS  => STUDENT (method invoked)
-// const func1 = student.getLohitBaby;
-// func1(); // GEC =< THIS => WINDOW (function call )
+// const call3 = () => {
+// console.log('Arrow' ,this)
+// }
 
-//Note-> 1  Normal Function (This concepts is only for remebering this value)
-// IF FUNCTION CALL THEN THIS  => WINDOW 
-// IF METHOD INVOKED THEN "THIS" WILL BE THE VALUE BEFORE DOT 
-
+// call3()
 
 
-const employee = {
-    firstName: 'Radar ',
-    lastName: 'Yuirt',
-    salary: 230000,
-    getSalary: function() {
-      console.log( this.salary) // ?? 
-      function call2 () {
-        console.log('inside getSalary salary' , this ) // ?? 
-      }
-      call2();
+
+// var employee = {
+//     firstName: 'Radar ',
+//     lastName: 'Yuirt',
+//     salary: 230000,
+//     getSalary: () => {
+//       console.log(this) // ?? 
+//       const call1  = () => {
+//         console.log('Function inside the getSalary' , this) // ??
+//     }
+//     call1() // 
+//     },
+//     getFullName() { 
+//         console.log(this.firstName +  this.lastName)  // ??  this => window
+//         const call1  = () => {
+//             console.log('Function inside the getFullName' , this) // ??
+//         }
+//         call1()  
+//     }
+// }
+
+// // employee.getSalary();
+
+// employee.getFullName()
+
+
+// var data =  employee.getFullName;
+// data(); //  this  => window
+
+// method invkoked
+// function call 
+// arrow take parent normal function this ofr window
+
+
+const students123 = {
+    name: "Vishal",
+    lastName: "Sharma",
+    college: "Newton",
+    printCollegeName: () => {
+      console.log("0=>", this.college);  
     },
-    getFullName() { 
-        console.log(this.firstName +  this.lastName)  // ??  this => employee
-        function call1 () {
-            console.log('Function inside the getFullName' , this) // ??
+    printName: function () {
+      console.log("1==>", this.name, this.lastName);  
+      function Func1() {
+        console.log("2==>", this.name);  
+        function func2() {
+          console.log("3==>", this.name);  
+          const fun3 = () => {
+            console.log("4=>", this.name);  
+          };
+          fun3();
         }
-        call1() //  this is the function call => this  => window (not strict mode in strict mode it will  be undefined)
-    }
-}
+        func2();
+      }
+      Func1(); 
+    },
+  };
+  students123.printName();  
+  students123.printCollegeName();
 
-employee.getFullName();
-employee.getSalary();
+  // function borrowing  => todo
+
+
+  // Javascript factory function  => the function which return the object
