d12781172038e8b1f24657645f44de8374d600d8
Vishal Sharma <785vishal443@gmail.com>
2024-05-15 21:36:30 +0530

    Promise start

---
+++ AsyncJavascript/Promises/index.js
@@ -0,0 +1,14 @@
+// Array 
+// String 
+// Number
+// Function 
+// Object 
+// Array 
+
+// array  => Array()
+
+const promise = new Promise((resolved , rejected)=>{
+  resolved(2)
+}); 
+
+console.log(promise)
