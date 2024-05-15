7d721996c2a1568ed74abf7de1339fa686dc0daf
Vishal Sharma <785vishal443@gmail.com>
2024-05-15 22:14:12 +0530

    promise chaining

--- AsyncJavascript/Promises/index.js
+++ AsyncJavascript/Promises/index.js
@@ -33,3 +33,56 @@ const pr1 = new Promise((resolve , reject)=>{
     reject('dont worry you will get better')
 })
 console.log(pr1)
+
+
+const pp1 = new Promise((resolve , reject)=>{
+
+})
+console.log(pp1)
+
+
+const pp2 = new Promise((resolve , reject)=>{
+  resolve('I called first')
+  reject('I will call later')
+})
+console.log(pp2)
+
+
+
+
+const promise1 = new Promise((resolved , rejected)=>{
+    setTimeout(()=>{
+      resolved([1,2,2,6,3,4,54,5])
+    } ,5000)
+})
+
+
+
+// promise chaining 
+// then  
+// catch
+// finally
+promise1.then((res)=>{
+  // if promise is resolved
+  console.log('success' , res)
+}).catch((err)=>{
+  //if promise is rejected
+  console.log('error' , res)
+})
+
+
+
+const promise2 = new Promise((resolved , rejected)=>{
+  setTimeout(()=>{
+    rejected("Error Occurred while calling the api.....")
+  } ,6000)
+})
+
+function success (res) {
+  console.log(res)
+}
+
+function error (error) {
+  console.log(error)
+}
+promise2.then(success).catch(error)
