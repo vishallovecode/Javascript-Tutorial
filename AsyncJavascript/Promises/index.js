a6cd4d25a530bc51f9c4cdab9789407f557850be
Vishal Sharma <785vishal443@gmail.com>
2024-05-15 22:43:41 +0530

    fetch start

--- AsyncJavascript/Promises/index.js
+++ AsyncJavascript/Promises/index.js
@@ -1,99 +1,140 @@
-// Array 
-// String 
-// Number
-// Function 
-// Object 
-// Array 
+// // Array 
+// // String 
+// // Number
+// // Function 
+// // Object 
+// // Array 
 
-// array  => Array()
+// // array  => Array()
 
-// const promise = new Promise((resolved , rejected)=>{
-//   resolved(2)
-// }); 
+// // const promise = new Promise((resolved , rejected)=>{
+// //   resolved(2)
+// // }); 
 
-// console.log(promise)
-//  {
+// // console.log(promise)
+// //  {
 
-// [[PromiseState]]: "fulfilled"
-// [[PromiseResult]]: 2
-//  }
+// // [[PromiseState]]: "fulfilled"
+// // [[PromiseResult]]: 2
+// //  }
 
 
-function callback (resolved , rejected) {
-  // first parameter is function which will use for resolving the promise
-  // second parameter is function which will use for rejecting the promise
-    resolved(234) // whatever we passed argument to these two function this willbecome the result of the promise object
+// function callback (resolved , rejected) {
+//   // first parameter is function which will use for resolving the promise
+//   // second parameter is function which will use for rejecting the promise
+//     resolved(234) // whatever we passed argument to these two function this willbecome the result of the promise object
 
+// }
+// const pr =  new Promise(callback);
+
+// console.log(pr)
+
+// const pr1 = new Promise((resolve , reject)=>{
+//     reject('dont worry you will get better')
+// })
+// console.log(pr1)
+
+
+// const pp1 = new Promise((resolve , reject)=>{
+
+// })
+// console.log(pp1)
+
+
+// const pp2 = new Promise((resolve , reject)=>{
+//   resolve('I called first')
+//   reject('I will call later')
+// })
+// console.log(pp2)
+
+
+
+
+// const promise1 = new Promise((resolved , rejected)=>{
+//     setTimeout(()=>{
+//       resolved([1,2,2,6,3,4,54,5])
+//     } ,5000)
+// })
+
+
+
+// // promise chaining 
+// // then  
+// // catch
+// // finally
+// promise1.then((res)=>{
+//   // if promise is resolved
+//   console.log('success' , res)
+// }).catch((err)=>{
+//   //if promise is rejected
+//   console.log('error' , err)
+// })
+
+
+
+// const promise2 = new Promise((resolved , rejected)=>{
+//   setTimeout(()=>{
+//     rejected("Error Occurred while calling the api.....")
+//   } ,6000)
+// })
+
+// function success (res) {
+//   console.log(res)
+// }
+
+// function error (error) {
+//   console.log(error)
+// }
+// promise2.then(success).catch(error).finally(()=>{
+//   console.log('Code cleaning perspective')
+// })
+
+
+// const neverEndingPromise = new Promise((resolved , rejected)=>{
+
+// })
+
+// neverEndingPromise.then(()=>{
+//   console.log('come here with success')
+// }).catch(()=>{
+//   console.log('come here with error')
+// })
+
+
+
+
+
+// // .then((success=>{
+
+// // }) , (error)=>{
+
+// // })
+
+// // then can take two callback function first is for success and second can be for error (this can be handle in .catch block)
+// // promise.then(
+// //   function(result) { /* handle a successful result */ },
+// //   function(error) { /* handle an error */ }
+// // );
+
+function fetchProduct() {
+  // fetch will return the promise
+  fetch('https://dummyjson.com/products').then((response)=>{
+    response.json().then((res)=>{
+      console.log(res)
+    })
+  }).catch((error)=>{
+    console.log('Error')
+  })
 }
-const pr =  new Promise(callback);
-
-console.log(pr)
-
-const pr1 = new Promise((resolve , reject)=>{
-    reject('dont worry you will get better')
-})
-console.log(pr1)
+fetchProduct()
 
 
-const pp1 = new Promise((resolve , reject)=>{
-
-})
-console.log(pp1)
-
-
-const pp2 = new Promise((resolve , reject)=>{
-  resolve('I called first')
-  reject('I will call later')
-})
-console.log(pp2)
-
-
-
-
-const promise1 = new Promise((resolved , rejected)=>{
-    setTimeout(()=>{
-      resolved([1,2,2,6,3,4,54,5])
-    } ,5000)
-})
-
-
-
-// promise chaining 
-// then  
-// catch
-// finally
-promise1.then((res)=>{
-  // if promise is resolved
-  console.log('success' , res)
-}).catch((err)=>{
-  //if promise is rejected
-  console.log('error' , res)
-})
-
-
-
-const promise2 = new Promise((resolved , rejected)=>{
-  setTimeout(()=>{
-    rejected("Error Occurred while calling the api.....")
-  } ,6000)
-})
-
-function success (res) {
-  console.log(res)
-}
-
-function error (error) {
-  console.log(error)
-}
-promise2.then(success).catch(error)
-
-
-const neverEndingPromise = new Promise((resolved , rejected)=>{
-
-})
-
-neverEndingPromise.then(()=>{
-  console.log('come here with success')
-}).catch(()=>{
-  console.log('come here with error')
-})
+// fetch(){
+//   if(apicallkhatam) {
+//     if(apigives error) {
+//       reject()
+//     } else {
+//       resolved(apiresult)
+//     }
+//   }
+// }
