4783a92f4118e9f13724756cd3b24b4fedb2e226
Vishal Sharma <785vishal443@gmail.com>
2024-05-23 21:36:39 +0530

    map fpolyfill

--- PolyFill/index.js
+++ PolyFill/index.js
@@ -1,68 +1,99 @@
 
 
-// 
+// // 
 
-// const data = {
-//   name: 'Hello',
-//   printName: function () {
-//     console.log(this.name)
+// // const data = {
+// //   name: 'Hello',
+// //   printName: function () {
+// //     console.log(this.name)
+// //   }
+// // }
+
+
+// // const bindFunc = data.printName.bind({name: 'Raj'})
+// // bindFunc() // Raj
+ 
+
+// // Polyfill for bind
+// Function.prototype.mybind = function(obj , ...args) {
+//     const func =  this;
+//     return function (...arg2) {
+//       func.apply(obj , [...args , ...arg2])
+//     }
+// }
+
+// function getExample(...args) {
+//     console.log(this.name , args)
+// }
+
+// const func = getExample.mybind({name: 'Hurrah'} , 1,2,3,4,5)
+// func(6,7,8,9)
+
+
+
+// // folyfill for call ?? 
+
+// Function.prototype.mycall = function(context , ...args) {
+//   context.func =  this; // args coming as rest opeartor and it is in array
+//   context.func(...args) //method invocation
+// }
+
+// // rest operator 
+// // spread operator 
+// // map filter , reduce
+// // race , all , allsettled , any
+// // promise 
+
+// function getRaja() {
+//   console.log(this.myname)
+// }
+
+// getRaja.mycall({myname: 'Raja hu me'})
+
+
+// const context = {
+//   myname: 'Raja hu me',
+//   func: function () {
+//     console.log(this.myname) //  this?? => context
 //   }
 // }
 
+// context.func()
 
-// const bindFunc = data.printName.bind({name: 'Raj'})
-// bindFunc() // Raj
- 
 
-// Polyfill for bind
-Function.prototype.mybind = function(obj , ...args) {
-    const func =  this;
-    return function (...arg2) {
-      func.apply(obj , [...args , ...arg2])
-    }
+// Function.prototype.myapply = function(context , args) {
+//   // args is already in array 
+//   context.func =  this;
+//   context.func(...args) // here we are spreading array
+// }
+
+
+
+// // Polyfill of map 
+
+Array.prototype.mymap = function (callback) {
+//  ?? 
+// this =>  array ??
+const array = this ;
+const result  = [];
+for(let i=0;i<array.length;i++) {
+  // result.push(callback(array[i] , index , array))
+  result[i] = callback(array[i] , i , array)
+}
+return result;
 }
 
-function getExample(...args) {
-    console.log(this.name , args)
-}
 
-const func = getExample.mybind({name: 'Hurrah'} , 1,2,3,4,5)
-func(6,7,8,9)
+const array  = [1,2,3,4,5,6];
 
+// const data = array.map((elem , index, array)=>{
+//   return elem*2
+//   });
 
+  //data= [2,4,6,8,10,12]
 
-// folyfill for call ?? 
+const data = array.mymap((elem , index, array)=>{
+return elem*2
+});
 
-Function.prototype.mycall = function(context , ...args) {
-  context.func =  this; // args coming as rest opeartor and it is in array
-  context.func(...args) //method invocation
-}
-
-// rest operator 
-// spread operator 
-// map filter , reduce
-// race , all , allsettled , any
-// promise 
-
-function getRaja() {
-  console.log(this.myname)
-}
-
-getRaja.mycall({myname: 'Raja hu me'})
-
-
-const context = {
-  myname: 'Raja hu me',
-  func: function () {
-    console.log(this.myname) //  this?? => context
-  }
-}
-
-context.func()
-
-
-Function.prototype.myapply = function(context , args) {
-  // args is already in array 
-  context.func =  this;
-  context.func(...args) // here we are spreading array
-}
+ console.log(data , 'data')
