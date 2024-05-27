9ff3a3de835dcc9d67db110b2e35742f31cb70c9
Vishal Sharma <785vishal443@gmail.com>
2024-05-27 22:01:15 +0530

    event loop

--- AsyncJavascript/index.js
+++ AsyncJavascript/index.js
@@ -74,3 +74,37 @@ for(let i=0;i<50000;i++) {
 }
 const endTime = new Date();
 console.log('EndTime',endTime)
+
+
+console.log('start')
+
+setTimeout(() => {
+  console.log('setTimeout')
+},0)
+
+Promise.resolve().then(() => {
+  console.log('resolve')
+})
+
+console.log('end')
+
+
+const promise = new Promise((resolve, reject) => {
+    console.log(1);
+    setTimeout(() => {
+      console.log("timerStart");
+      resolve("success"); //
+      console.log("timerEnd");
+    }, 0);
+    console.log(2);
+  });
+
+  promise.then((res) => {
+    console.log(res);
+  });
+
+  setTimeout(()=>{
+    console.log("Are!!")
+  } ,0)
+
+  console.log(4);
