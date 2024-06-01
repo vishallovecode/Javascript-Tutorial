2317b9d8ed69e75f764c789cd7ee18913f8859e1
Vishal Sharma <785vishal443@gmail.com>
2024-06-01 22:24:20 +0530

    local vs session storage

--- WebStorage/index.js
+++ WebStorage/index.js
@@ -1,15 +1,38 @@
 async function onSubmit (event) {
-  const payload = {};
-  event.preventDefault()
+  const payload = {}; // empty object
+  event.preventDefault() // we want to stop default behaviour of form component , like onSubmit reloading page
   payload.username =  document.getElementById('userNameId')?.value;
   payload.password =  document.getElementById('passwordId')?.value;
+  // this api is expecting the payload or body in given format
+//   {
+//     username: 'emilys',
+//     password: 'emilyspass',
+// }
   const data  = await fetch('https://dummyjson.com/auth/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
   })
-
   const resData =  await data.json();
-  localStorage.setItem('userDetails' ,JSON.stringify(resData) )
-  
+  // setting the value to localStorage
+  localStorage.setItem('userDetails' ,JSON.stringify(resData) )  // 10mb
+    // setting the value to session
+
+  sessionStorage.setItem('userDetails' ,JSON.stringify(resData) ) // 5mb
 }
+
+
+function getUserToken () {
+  //  getting the value from localStorage
+  const userDetails = localStorage.getItem('userDetails');
+    //  getting the value from session
+  const userDetails1 = sessionStorage.getItem('userDetails');
+  const JSon =  JSON.parse(userDetails);
+  console.log(JSon.token)
+}
+
+function removeItem() {
+  sessionStorage.removeItem('userDetails')
+  localStorage.removeItem('userDetails')
+}
+
