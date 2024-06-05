9a80cd015d9584b637f123f8fe7603303712d8d5
Vishal Sharma <785vishal443@gmail.com>
2024-06-05 23:24:51 +0530

    Movie card

--- Project/Movie_Deck/movie.js
+++ Project/Movie_Deck/movie.js
@@ -1,6 +1,7 @@
 // 
 const base_url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US';
-async function handleSearch (event) {
+const base_img_url = 'https://image.tmdb.org/t/p/original'
+ function handleSearch (event) {
   event.preventDefault()
   // here we need to get data first from the input value and on the basis of that call the api right/
   const movieValue = document.getElementById('movieName')?.value;
@@ -13,10 +14,62 @@ async function getAllMovies(page=1) {
   try {
     const data = await fetch(`${base_url}&page=${page}`)
     const moviesList =  await data.json();
-    console.log(moviesList)
- } catch(err){
+    const movieList = document.getElementById('movieList');
+    moviesList.results.forEach((movie , index) => {
+      const container = createMovieCard(movie)
+      movieList.appendChild(container)
+    });
 
+ } catch(err){
+  console.log(err)
  }
 }
 
 getAllMovies()
+
+
+function createMovieCard(movie) {
+  const cardContainer =  document.createElement('div');
+  cardContainer.className='movie-card'
+  // header
+  const header = document.createElement('div');
+  header.style.width= '300px'
+  header.style.height= '200px'
+  const img = document.createElement('img')
+  img.className ='card-img'
+  img.src= `${base_img_url}${movie.poster_path}`
+  header.append(img)
+  
+  cardContainer.appendChild(header)
+
+  // body section
+  const body = document.createElement('div');
+  body.className ='card-body'
+  const row1 = document.createElement('div');
+  row1.className ='title'
+  row1.textContent=  movie.original_title
+  body.appendChild(row1)
+  const row2 = document.createElement('div');
+  row2.className ='content'
+  // content
+  const content = document.createElement('div');
+  content.className='footer'
+ 
+  const span = document.createElement('span');
+  span.textContent= `Votes: ${movie.vote_count}`
+  const span2 = document.createElement('span');
+  span2.textContent= `Rating: ${movie.vote_average}`
+
+  content.appendChild(span)
+  content.appendChild(span2)
+  row2.appendChild(content)
+
+  // heart section
+  const heart = document.createElement('div')
+  heart.textContent = '&#9829;'
+  row2.appendChild(heart)
+  body.appendChild(row2)
+  cardContainer.append(body)
+
+  return cardContainer;
+}
